8381b9460a05ec52e5581b0aa024b727d12ab921
anatoliy1984 <zozav@ukr.net>
2017-12-20 20:22:27 +0200

    ok

--- app/pizzaController.js
+++ app/pizzaController.js
@@ -6,13 +6,28 @@ app.controller('pizzaController', [
             availableToppings: ['Cheese', 'Pepperoni', 'Bacon', 'Pineapple', 'Sausage', 'Ham', 'Chicken',
                 'Mushrooms', 'Onion', 'Olives', 'Green Peppers'
             ],
-            toppings: []
-
+            toppings: [],
+            sAlert: false,
+            sNoResult: true
+        };
+        $scope.closeAlert = function() {
+            $scope.model.sAlert = false;
+            $scope.model.sNoResult = false;
         };
         $scope.addTopping = function(topping) {
             $scope.model.toppings.push(topping);
             $scope.model.search = null;
-        }
-
+            $scope.model.sAlert = true;
+            $scope.model.sNoResult = false;
+        };
+        $scope.$watch('filtered', function(newValue, oldValue, scope) {
+            //Do anything with $scope.letters
+            if (newValue == undefined)
+                $scope.model.sNoResult = false;
+            else {
+                $scope.model.sNoResult = (newValue.length < 1) && ($scope.model.search);
+                //console.log(n)
+            }
+        });
     }
 ]);
